a36e2dfd6a89678e51cd980fc0fefdd423d6a5d1
Rodolfo Carvajal <rodolfo.carvajal@gmail.com>
2013-12-16 23:37:41 -0500

    Update config.js
    Added a post

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 var config = {
   title: 'Grite',
-  navbar: [ { text: 'Fork me on Github',
-              link: 'https://github.com/sankha93/grite' },
-            { text: 'Documentation',
-              link: 'https://github.com/sankha93/grite/wiki/Documentation'} ],
-  posts: [ '5711471',
-           '5711492'],
+  navbar: [{ text: 'Home',
+              link: 'http://www2.isye.gatech.edu/~rcarvajal3'} ],
+  posts: [ '8000111'],
   atOnce: 5,
   footerText: "Grite has been made with love by Sankha. Follow him on Twitter at <a href=\"https://twitter.com/sankha93\">@sankha93</a>."
 };
